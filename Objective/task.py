d6ab87ae4ee2de36a6d1cc202ea754efb7b2121a
Near <singleangel@vip.qq.com>
2023-09-22 14:36:13 +0800

    feat:add objectives

---
+++ Objective/task.py
@@ -0,0 +1,20 @@
+class task(object):
+    
+    def __init__(
+        self,
+        input_data_size: float,
+        cqu_cycles: float,
+        deadline: float,
+        ) -> None:
+        self._input_data_size : float = input_data_size
+        self._cqu_cycles : float = cqu_cycles
+        self._deadline : float = deadline
+        
+    
+def generate_task_set(
+    min_input_data_size: float,
+    max_input_data_size: float,
+    
+):
+    return list(task)
+    pass
