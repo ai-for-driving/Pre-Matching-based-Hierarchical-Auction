eb2ede853db41489e41a41b99f6e0bf14fcdc9e0
llf-cpu <619419128@qq.com>
2023-09-26 11:39:45 +0800

    add some edge_node

--- Pre-Matching-based-Hierarchical-Auction/Objective/edge_node.py
+++ Pre-Matching-based-Hierarchical-Auction/Objective/edge_node.py
@@ -1,5 +1,7 @@
 from typing import Any, List
 import numpy as np
+import random
+import math
 from Objective.mobility import mobility
 from Objective.vehicle import vehicle
 
@@ -11,17 +13,21 @@ class edge_node(object):
         storage_capability: float,
         communication_range: float,
     ) -> None:
-        pass
+        self_mobility: mobility = mobility
+        self_computing_capability: float = computing_capability
+        self_storage_capability : float = storage_capability
+        self_communication_range : float = communication_range
+
     
     def __getattribute__(self, __name: str) -> Any:
         pass
     
     def get_availiable_computing_capability(self) -> float:
-        pass
+        return self.computing_capability
     
     # added by near, the edge node should have storage capability
     def get_availiable_storage_capability(self) -> float:
-        pass
+        return self.storage_capability
     
     def set_consumed_computing_capability(self, consumed_computing_capability: float, duration: int) -> None:
         pass
@@ -29,6 +35,10 @@ class edge_node(object):
     def set_consumed_storage_capability(self, consumed_storage_capability: float, duration: int) -> None:
         pass
     
+    def calculate_distance(self, x1, x2, y1, y2, current_time) -> float:
+        pass
+        
+
 
 def generate_edge_nodes(
     mobilities: List[mobility],
@@ -36,9 +46,18 @@ def generate_edge_nodes(
     max_computing_capability: float,
     min_storage_capability: float,
     max_storage_capability: float,
+    communication_range : float,
     distribution: str,
 ) -> List(edge_node):
-    pass
+    edge_nodes = []
+    for mobility in mobilities:
+        if distribution == "uniform" :
+            computing_capability = random.uniform(min_computing_capability,max_computing_capability)
+            storage_capability = random.uniform(min_storage_capability,max_storage_capability)
+        else:
+            pass
+        edge_node_obj = edge_node(mobility,computing_capability,storage_capability,communication_range)
+        edge_nodes.append(edge_node_obj)
 
 def get_vehicles_under_coverage_of_edge_nodes(
     vehicles: List[vehicle],
@@ -51,7 +70,16 @@ def get_vehicles_under_coverage_of_edge_nodes(
     result = np.zeros((num_vehicles, num_edge_nodes))
     for i in range(num_vehicles):
         for j in range(num_edge_nodes):
-            pass
+            vehicle = vehicles[i]
+            edge_node = edge_nodes[j]
+            distance = calculate_distance(edge_node.mobility.location_x, 
+                                          vehicle.mobility.location_x, 
+                                          edge_node.mobility.location_y,
+                                          vehicle.mobility.location_y,
+                                          now)
+            if(distance <= V2I_distance):
+                result[i,j] = 1
+
     return result
 
 def generate_edge_nodes_test():
